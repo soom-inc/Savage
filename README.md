ca24c9d5e33c9bb5b89f06a54bdb9f22e2ffb378
henryzlo <henryzlo@gmail.com>
2015-03-11 16:10:13 -0400

    added readme file, slimmed down the example index.html.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Savage
 SVG templating engine written in Javascript.
+
+Usage:
+```
+s = Savage(SVGTemplatingString)  // returns SVG template object
+s.getFields()                    // returns a dictionary of field names and values
+s.setFields(dictionary)          // can update any number of fields
+s.getTypes()                     // returns types of each field
+s.render()                       // returns compiled SVG string
+```
+
+`SVGTemplatingString` should be SVG (should start and end with `svg` tags).  Fields must be typed and specified with the format below:
+```
+{{ typeString myTextField }}
+```
+
+Example:
+```
+<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg"><g><title>background</title><rect fill="{{ color bg_color }}" height="400" width="400" /><text y="200" x="200">{{ string text }}</text></g></svg>
+```
+
+Notes:
+
+- You MUST eliminate all illegal javascript tokens in the SVG string, e.g. newlines, lest you get an `Unexpected token ILLEGAL` error.
+- As of right now, type can be any string (we may implement type-checking in the future).
---
+++ savage.js
@@ -0,0 +1,51 @@
+function Savage(SVGTemplate) {
+    this.template = SVGTemplate
+    this.fields = null
+    this.types = null
+
+    this.getFields = function() {
+	return this.fields
+    }
+
+    this.setFields = function(newFields) {
+	for (var param in newFields) {
+	    // TODO: type-based validation
+	    if (param in this.fields) {
+		this.fields[param] = newFields[param]
+	    } else {
+		throw "KeyError: Field " + param + " does not exist."
+	    }
+	}
+    }
+
+    this.getTypes = function() {
+	return this.types
+    }
+
+    this.render = function() {
+	output = this.template
+	for (var param in this.fields) {
+	    re = new RegExp("{{\\s*" + this.types[param] + "\\s+" + param + "\\s*}}")
+	    output = output.replace(re, this.fields[param])
+	}
+	return output
+    }
+
+    // private helper function to parse string for fields, types
+    construct = function() {
+	this.fields = {}
+	this.types = {}
+	re = /\{\{(.*?)\}\}/g
+	// must loop over each match
+	while (parameter = re.exec(this.template)) {
+	    tokens = parameter[1].match(/(\S+)/g)
+	    type = tokens[0]
+	    name = tokens[1]
+	    this.fields[name] = ""
+	    this.types[name] = type
+	}
+    }
+
+    construct()
+    return this
+}
